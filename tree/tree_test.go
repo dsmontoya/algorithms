6e6960c24ad99bd4c37f7c87833ff7a4caa644df
Daniel Montoya <dsmontoyam@gmail.com>
2020-05-20 19:05:18 -0500

    add new test tree

--- tree/tree_test.go
+++ tree/tree_test.go
@@ -18,3 +18,33 @@ var testTree = &BinaryNode{
 		},
 	},
 }
+
+var testTree2 = &BinaryNode{
+	1,
+	&BinaryNode{
+		2,
+		&BinaryNode{
+			4,
+			nil,
+			nil,
+		},
+		&BinaryNode{
+			5,
+			nil,
+			nil,
+		},
+	},
+	&BinaryNode{
+		3,
+		&BinaryNode{
+			6,
+			nil,
+			nil,
+		},
+		&BinaryNode{
+			7,
+			nil,
+			nil,
+		},
+	},
+}
