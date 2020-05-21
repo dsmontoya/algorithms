aa1f0c6e3ea702b652b9b39bc76cf273cb99b7e1
Daniel Montoya <dsmontoyam@gmail.com>
2020-05-20 19:07:21 -0500

    bfs traversal

--- tree/bfs_test.go
+++ tree/bfs_test.go
@@ -12,6 +12,7 @@ func TestBinaryNode_BFS(t *testing.T) {
 		want [][]int
 	}{
 		{"1", testTree, [][]int{{1}, {2}, {3, 4}, {5, 6}}},
+		{"2", testTree2, [][]int{{1}, {2, 3}, {4, 5, 6, 7}}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
